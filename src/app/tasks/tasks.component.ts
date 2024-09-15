2c38d0c0a7e84604544b023f064cde99a3943c5d
Prajval <prajvalhl@gmail.com>
2024-09-15 21:06:17 +0530

    updated to use task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { dummyTasks } from './dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -9,5 +10,10 @@ import { TaskComponent } from './task/task.component';
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
+  @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+
+  get selectedUserTasks() {
+    return dummyTasks.filter((task) => task.userId === this.userId);
+  }
 }
