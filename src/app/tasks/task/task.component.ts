2084ddaad7a823aa749f458dfe162aa6e80880dd
Prajval <prajvalhl@gmail.com>
2024-10-06 22:04:55 +0530

    refactor: updated component

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,14 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { type Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
-  @Input() task?: Task;
+  @Input() task!: Task;
+  @Output() completed = new EventEmitter<string>();
+
+  onTaskComplete() {
+    this.completed.emit(this.task.id);
+  }
 }
